f94ba3fce37823c9b0c8e11fb27108ed8fefbe24
vscholl <victoria.scholl@colorado.edu>
2020-05-18 12:41:58 -0600

    updated column names, added fire size and year to table

--- create_fire_summary_table.R
+++ create_fire_summary_table.R
@@ -8,32 +8,36 @@ fire_filename <- here::here("data/fire_stats_test.geojson")
 fire_data <- sf::st_read(fire_filename)
 
 # adjust the data type and/or precision of some fields
-fire_data$id <- as.numeric(as.character(fire_data$id))  
+fire_data$id <- as.character(fire_data$id)
+fire_data$Acres <- as.integer(fire_data$Acres)
 fire_data$lodgepole <- round(as.numeric(as.character(fire_data$lodgepole)) * 100, digits = 1)
 fire_data$ponderosa <- round(as.numeric(as.character(fire_data$ponderosa)) * 100, digits = 1)
 fire_data$spruceFir <- round(as.numeric(as.character(fire_data$spruceFir)) * 100, digits = 1)
-fire_data$dstrb_brnd <- round(as.numeric(as.character(fire_data$dstrb_brnd)) * 100, digits = 1)
-fire_data$dstrb_unsp <- round(as.numeric(as.character(fire_data$dstrb_unsp)) * 100, digits = 1)
-fire_data$dstrb_logd <- round(as.numeric(as.character(fire_data$dstrb_logd)) * 100, digits = 1)
-fire_data$regen_dist <- round(as.numeric(as.character(fire_data$regen_dist)) * 100, digits = 1)
-fire_data$regen_harv <- round(as.numeric(as.character(fire_data$regen_harv)) * 100, digits = 1)
+fire_data$disturbed_burned <- round(as.numeric(as.character(fire_data$disturbed_burned)) * 100, digits = 1)
+fire_data$disturbed_unspecific <- round(as.numeric(as.character(fire_data$disturbed_unspecific)) * 100, digits = 1)
+fire_data$disturbed_logged <- round(as.numeric(as.character(fire_data$disturbed_logged)) * 100, digits = 1)
+fire_data$regenerating_disturbed <- round(as.numeric(as.character(fire_data$regenerating_disturbed)) * 100, digits = 1)
+fire_data$regenerating_harvested <- round(as.numeric(as.character(fire_data$regenerating_harvested)) * 100, digits = 1)
 
 # create a table to summarize attributes of interest per fire 
 fire_table <- as.data.frame(fire_data) %>% 
   # select columns of interest
-  dplyr::select(id, lodgepole, ponderosa, spruceFir, dstrb_brnd, dstrb_unsp, dstrb_logd, regen_dist, regen_harv) %>%
+  dplyr::select(Fire_Name, Year, Acres, lodgepole, ponderosa, spruceFir, disturbed_burned, disturbed_unspecific, 
+                disturbed_logged, regenerating_disturbed, regenerating_harvested) %>%
   # reorder the rows based on multiple variables 
-  dplyr::arrange(desc(lodgepole), desc(ponderosa), desc(spruceFir), desc(dstrb_brnd)) %>%
+  dplyr::arrange(desc(lodgepole), desc(ponderosa), desc(spruceFir), desc(disturbed_burned)) %>%
   # rename the columns for interpretation
-  dplyr::rename("Fire id" = id, 
+  dplyr::rename("Fire Name" = Fire_Name, 
+                "Year" = Year,
+                "Acres" = Acres,
                 "Lodgepole %" = lodgepole,
                 "Ponderosa %" = ponderosa,
                 "Spruce Fir %" = spruceFir,
-                "Disturbed Burned %" = dstrb_brnd,
-                "Disturbed Unspecified %" = dstrb_unsp,
-                "Disturbed Logged %" = dstrb_logd,
-                "Regenerating Disturbed %" = regen_dist,
-                "Regenerating Harvested %" = regen_harv) 
+                "Disturbed Burned %" = disturbed_burned,
+                "Disturbed Unspecified %" = disturbed_unspecific,
+                "Disturbed Logged %" = disturbed_logged,
+                "Regenerating Disturbed %" = regenerating_disturbed,
+                "Regenerating Harvested %" = regenerating_harvested) 
 
 kableExtra::kable(fire_table) %>%
   kableExtra::kable_styling(bootstrap_options = "striped", 
