e5aee03f26000df67ae8f3c68f5de8777b5d344f
vscholl <victoria.scholl@colorado.edu>
2020-06-08 14:12:37 -0600

    the shp file is not being created correctly

--- gpkg_to_shp.R
+++ gpkg_to_shp.R
@@ -8,3 +8,7 @@ gpkg <- sf::st_read("~/Downloads/modis_event_polygons_cus.gpkg")  #FIRED data
 
 # write to .shp
 sf::st_write(sf::st_write(obj = gpkg, dsn = here::here("data/data_output/FIRED.shp")))
+
+
+# read the shapefile to make sure it was written correctly
+test <- sf::st_read("data/data_output/FIRED.shp")
