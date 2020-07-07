1fbe2f53e5e579945b3dcff0b350654758534857
vscholl <victoria.scholl@colorado.edu>
2020-07-07 14:16:12 -0600

    need to convert to geog coords. debugging error now when writing to shp

--- gpkg_to_shp.R
+++ gpkg_to_shp.R
@@ -3,12 +3,18 @@
 library(sf)
 library(here)
 
+# check versions of sf packages
+sf::sf_extSoftVersion()
+
 # read .gpkg file with geometry
 gpkg <- sf::st_read("~/Downloads/modis_event_polygons_cus.gpkg")  #FIRED data
 
-# write to .shp
-sf::st_write(sf::st_write(obj = gpkg, dsn = here::here("data/data_output/FIRED.shp")))
+# transform geometry coordinates from "MODIS sinu" to geog coords EPSG:4326 (lat, lon)
+gpkg <- sf::st_transform(gpkg, 4326)
 
+# write to .shp
+sf::st_write(obj = gpkg, 
+             dsn = here::here("data/data_output/FIRED.shp"))
 
 # read the shapefile to make sure it was written correctly
-test <- sf::st_read("data/data_output/FIRED.shp")
+test <- sf::st_read(here::here("data/data_output/FIRED.shp"))
